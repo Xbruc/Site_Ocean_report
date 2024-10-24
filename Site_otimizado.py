ab8cb74b0b04c0f822b840f5ff51cb92577deee8
Wesley Lima <86086577+Xbruc@users.noreply.github.com>
2024-10-24 11:24:34 -0300

    Update Site_otimizado.py

--- Site_otimizado.py
+++ Site_otimizado.py
@@ -1,10 +1,8 @@
 
 import streamlit as st
 import pandas as pd 
-import plotly.express as px
 import plotly.graph_objects as go
 import plotly.express as px
-import plost 
 import utm
 from datetime import datetime
 import calendar
